042a2242bff909f47342de484fda4817754e77ad
kauankajitani <kauankajitani02@gmail.com>
2022-05-11 15:43:11 -0300

    NavBar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
+import NavBar from 'components/navbar';
+import { ReactComponent as GitHubIcon } from 'C:/Projetos/dsmovies/frontend/src/assets/img/github.svg'
 function App() {
   return (
-    <h1>Página Principal</h1>
+    <NavBar/>
   );
 }
 
