e5016c5076776d81927a393fe7c767f241e18f0c
kauankajitani <kauankajitani02@gmail.com>
2022-05-15 20:39:09 -0300

    descricao

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as StarEmpty } from 'C:/Projetos/dsmovies/frontend/src/assets/img/star-empty.svg'
+import { ReactComponent as StarEmpty } from'C:/Projetos/dsmovies/frontend/src/assets/img/star-empty.svg'
 import { ReactComponent as StarFull } from 'C:/Projetos/dsmovies/frontend/src/assets/img/star-full.svg'
 import { ReactComponent as StarHalf } from 'C:/Projetos/dsmovies/frontend/src/assets/img/star-half.svg'
 import './style.css'
