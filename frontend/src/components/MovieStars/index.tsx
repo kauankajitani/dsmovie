9083c62abf70a1d5f7dd6ee41723747d36ecec25
kauankajitani <kauankajitani02@gmail.com>
2022-05-15 21:09:38 -0300

    descricao

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -1,6 +1,6 @@
-import { ReactComponent as StarEmpty } from 'C:/Projetos/dsmovies/frontend/src/assets/img/star-empty.svg'
-import { ReactComponent as StarFull } from 'C:/Projetos/dsmovies/frontend/src/assets/img/star-full.svg'
-import { ReactComponent as StarHalf } from 'C:/Projetos/dsmovies/frontend/src/assets/img/star-half.svg'
+import { ReactComponent as StarFull } from 'assets/img/star-full.svg';
+import { ReactComponent as StarHalf } from 'assets/img/star-half.svg';
+import { ReactComponent as StarEmpty } from 'assets/img/star-empty.svg';
 import './style.css'
 
 function MovieStars() {
